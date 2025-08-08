307104215f023a7740ee62523b24516abe421d26
Toufic <khaizarantoufic@gmail.com>
2025-08-08 16:35:52 +0300

    Fix build permissions and dependencies

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <section className="relative h-screen w-full z-0">
+    <section className="relative h-[120vh] w-full z-0 overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <img
           src="/images/hero-medical.png"
           alt="Hero"
           className="w-full h-full object-cover"
+          style={{ objectPosition: 'center 40%' }} // Lowered the image to better reveal bottom content
         />
         <div className="absolute inset-0 bg-black/40" />
       </div>
 
-      {/* Empty placeholder (you can remove this entirely if not needed) */}
+      {/* Empty space to preserve height */}
       <div className="relative z-10 h-full" />
 
-      {/* Bottom Wave */}
+      {/* Smaller Bottom Wave */}
       <div className="absolute bottom-0 w-full overflow-hidden leading-none z-10">
         <svg
           viewBox="0 0 1200 50"
           preserveAspectRatio="none"
-          className="w-full h-[50px]"
+          className="w-full h-[25px]" // Smaller wave height
         >
           <path
             d="M0,0 C300,50 900,50 1200,0 L1200,50 L0,50 Z"
