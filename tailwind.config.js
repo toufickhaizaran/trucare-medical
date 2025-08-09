1cece6778a21540d216940144b1c8609f4a23825
Toufic <khaizarantoufic@gmail.com>
2025-08-09 13:07:32 +0300

    Add GitHub Pages deploy workflow #1 + switch Tailwind/PostCSS to .cjs and ignore dist

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.cjs
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
