fd65fab35ca81e96a67d4c08fdb4187c751e02fe
Toufic <khaizarantoufic@gmail.com>
2025-08-11 15:13:59 +0300

    User site: BrowserRouter without basename; Vite base=/

---
+++ .vite/deps/react-dom.js
@@ -0,0 +1,7 @@
+import {
+  require_react_dom
+} from "./chunk-2JRVN4QV.js";
+import "./chunk-L2H5Y3CP.js";
+import "./chunk-UXIASGQL.js";
+export default require_react_dom();
+//# sourceMappingURL=react-dom.js.map
